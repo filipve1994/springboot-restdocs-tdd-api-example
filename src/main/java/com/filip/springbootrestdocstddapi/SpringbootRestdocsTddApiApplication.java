073f460f63fc9394c2e39c97d91ffd61a6b98abf
Filip Vanden Eynde <filipve1994@gmail.com>
2020-01-30 08:34:08 +0100

    first commit

---
+++ src/main/java/com/filip/springbootrestdocstddapi/SpringbootRestdocsTddApiApplication.java
@@ -0,0 +1,13 @@
+package com.filip.springbootrestdocstddapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestdocsTddApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestdocsTddApiApplication.class, args);
+	}
+
+}
